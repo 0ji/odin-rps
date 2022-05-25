c02f0215158c218f3e73f4eca14d1a0ecd7d2141
Koji <dinokoji@gmail.com>
2022-05-25 19:06:14 -0400

    Add function with logic for playing a round

--- rps.js
+++ rps.js
@@ -6,4 +6,41 @@ function computerPlay() {
     return selections[choice];
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    console.log("Player: " + playerSelection + ", Computer: " + computerSelection)
+
+    if (playerSelection == computerSelection) {
+        return "Tie! Both chose " + playerSelection; 
+    }
+
+    switch(playerSelection) {
+        case 'rock':
+            switch (computerSelection) {
+                case 'paper':
+                    return "You lose! " + computerSelection + " beats " + playerSelection;
+                case 'scissors':
+                    return "You win! " + playerSelection + " beats " + computerSelection;
+            }
+        case 'paper':
+            switch (computerSelection) {
+                case 'rock':
+                    return "You win! " + playerSelection + " beats " + computerSelection;
+                case 'scissors':
+                    return "You lose! " + computerSelection + " beats " + playerSelection;
+            }
+        case 'scissors':
+            switch (computerSelection) {
+                case 'rock':
+                    return "You lose! " + computerSelection + " beats " + playerSelection;
+                case 'paper':
+                    return "You win! " + playerSelection + " beats " + computerSelection;
+
+            }
+    }
+}
+
+for (let i=0; i<5; i++) {
+    console.log(playRound(computerPlay(), computerPlay()));
+}
